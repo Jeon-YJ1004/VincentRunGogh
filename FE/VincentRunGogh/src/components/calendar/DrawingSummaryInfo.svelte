f163b4d86c0934f52899cf5ca627063a5ea5af30
THIP-Y <nine14141732@gmail.com>
2024-09-19 22:53:51 +0900

    infra frontend test01

--- FE/VincentRunGogh/src/components/calendar/DrawingSummaryInfo.svelte
+++ FE/VincentRunGogh/src/components/calendar/DrawingSummaryInfo.svelte
@@ -25,7 +25,7 @@
     justify-content: space-between;
     align-items: center;
     background: white;
-    padding: 10px;
+    padding: 11px;
     border-radius: 8px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   }
